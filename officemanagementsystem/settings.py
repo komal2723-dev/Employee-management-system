ea53839f0b0a3bc4628e21be098e4ba2375f0239
komal2723-dev <komalg2723@gmail.com>
2025-02-03 13:55:08 +0530

    first commit

--- officemanagementsystem/settings.py
+++ officemanagementsystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bhdr#)9wpqbbxt#i)b!rxhsa7+lqfzrsn4t1_x1ptu+pvhogt@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
